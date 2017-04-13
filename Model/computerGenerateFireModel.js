2bfd23f51fd36238cc6a23ed5586383dda173874
Przemysław Juśkiewicz <nyweron@gmail.com>
2017-04-13 20:21:01 +0200

    remember computer shoots, remove value from allCells

--- Model/computerGenerateFireModel.js
+++ Model/computerGenerateFireModel.js
@@ -1,6 +1,6 @@
 const computerGenerateFireModel = {
 
-
+    retTargetValue: "",
 
     //Generate all 99 cells.
     generateAllCells: function() {
@@ -27,6 +27,13 @@ const computerGenerateFireModel = {
 
     //Computer check target with all playersShip
     checkComputerHitPlayerShip: function(tar) {
+        console.log("checkComputerHitPlayerShip.tar " + tar);
+        //remember all shoots, and remove value from allCells. Thanks that when I'm debug, I'm sure that computer never use the same value.
+        let target = this.rememberComputerShots(tar);
+        console.log("checkComputerHitPlayerShip.target " + target);
+        //remove target from allCells. Computer never choose the same value
+        this.removeOneCellFromAllCells(target);
+
         for (let i = 0; i < playerModel.ships.length; i++) {
             for (let j = 0; j < playerModel.ships[i].location.length; j++) {
                 if (playerModel.ships[i].location[j] == tar) {
@@ -37,6 +44,33 @@ const computerGenerateFireModel = {
             }
         }
         return tar;
+    },
+
+    rememberComputerShots: function(target) {
+        console.log("rememberComputerShots.target " + target);
+        let aim = parseInt(target);
+        console.log("rememberComputerShots.aim " + aim);
+        let valueFromIndex = computerGenerateFireModel.allCells[aim];
+        console.log("rememberComputerShots.valueFromIndex " + valueFromIndex);
+        //remember all computer shoots
+        computerGenerateFireModel.shoots.push(valueFromIndex);
+        retTargetValue = valueFromIndex;
+
+        return valueFromIndex;
+    },
+
+    removeOneCellFromAllCells: function(target) {
+        console.log("removeOneCellFromAllCells.target " + target);
+
+        let index = computerGenerateFireModel.allCells.indexOf(target);
+        console.log("removeOneCellFromAllCells.index " + index);
+        if (index >= 0) {
+            console.log("removeOneCellFromAllCells " + computerGenerateFireModel.allCells);
+            //remove one row in array, remove value
+            computerGenerateFireModel.allCells.splice(index, 1);
+            console.log("removeOneCellFromAllCells " + computerGenerateFireModel.allCells);
+
+        }
     },
 
     //If computer hit player ship then mark those ship hit=1
